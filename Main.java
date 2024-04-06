f8b157a2b3af694fe81385b384cff0e6efd5c23b
Hecate-vv <wdobrodziej@gmail.com>
2024-04-06 19:10:22 +0200

    zad4

--- Main.java
+++ Main.java
@@ -1,16 +1,24 @@
+import java.util.ArrayList;
+
 import geometry.Circle;
 import geometry.Point;
 import geometry.ColoredCircle;
 
 public class Main {
     public static void main(String[] args) {
-        Point center = new Point(1, 2);
-        Circle circle = new Circle(center, 5);
-        System.out.println("Współrzędne środka koła:(" + circle.getCenter().getX() +"," + circle.getCenter().getY() +")");
-        System.out.println("Promień:" + circle.getRadius());
-        System.out.println("Obwówd:" + circle.calculatePerimeter());
-        ColoredCircle coloredCircle = new ColoredCircle(center, 5, "Fiolet");
-        System.out.println("Kolor: " + coloredCircle.getColor());
+        ArrayList<Object> circles = new ArrayList<>();
+        circles.add(new Circles(4));
+        circles.add(new Circles(8));
+        circles.add(new ColoredCircle(null, 3, "Red"));
+        circles.add(new ColoredCircle(null, 1, "Blue"));
+        for (Object obj : circles) {
+            if (obj instanceof ColoredCircle) {
+                ColoredCircle coloredCircle = (ColoredCircle) obj;
+                System.out.println("Area: " + coloredCircle.calculateArea());
+                System.out.println("Color: " + coloredCircle.getColor());
+            } else if (obj instanceof Circle) {
+                Circle circle = (Circle) obj;
+                System.out.println("Area: " + circle.calculateArea());
     }
 
 }
