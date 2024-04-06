b7da7bc81822d059b018c58c202993bb4a3b4b32
Hecate-vv <wdobrodziej@gmail.com>
2024-04-06 19:21:24 +0200

    zad6

--- Main.java
+++ Main.java
@@ -1,24 +1,8 @@
-import java.util.ArrayList;
-
-import geometry.Circle;
-import geometry.Point;
-import geometry.ColoredCircle;
+import geometry.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Object> circles = new ArrayList<>();
-        circles.add(new Circles(4));
-        circles.add(new Circles(8));
-        circles.add(new ColoredCircle(null, 3, "Red"));
-        circles.add(new ColoredCircle(null, 1, "Blue"));
-        for (Object obj : circles) {
-            if (obj instanceof ColoredCircle) {
-                ColoredCircle coloredCircle = (ColoredCircle) obj;
-                System.out.println("Area: " + coloredCircle.calculateArea());
-                System.out.println("Color: " + coloredCircle.getColor());
-            } else if (obj instanceof Circle) {
-                Circle circle = (Circle) obj;
-                System.out.println("Area: " + circle.calculateArea());
+        Square square = new Square(3);
+        System.out.println(square.calculateArea());
     }
-
 }
